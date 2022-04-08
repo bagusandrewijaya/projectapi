4beafaed676b20297b594977806e6a56c511bbd8
bagusandre <bagusandrewijaya19@gmail.com>
2022-04-08 18:27:11 +0700

    update data

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req,res){
     response.ok("connection ok!",res)
 };
+
+//show data from mysqli
+exports.showdatam = function(req,res){
+    connection.query('select * from tb_mahasiswa',function(error,rows,fields){
+        if (error) {
+            connection.log(error);
+        }else{
+            response.ok(rows,res)
+        }
+    });
+};
--- routes.js
+++ routes.js
@@ -4,4 +4,6 @@ module.exports = function(app){
     var myjson = require('./controller');
     app.route('/')
         .get(myjson.index);
+    app.route('/tampil')
+        .get(myjson.showdatam);
 };
